0bf68b7529a98b97c910252ee1ba31bde495481d
Charly77955 <desantiscarlos95@gmail.com>
2022-08-18 21:40:28 -0300

    feat: start code

---
+++ inmoApp/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text, SafeAreaView} from 'react-native'
+
+export default function Main(){
+    return (
+        <SafeAreaView>
+            <View>
+            <Text> Nueva app react</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
