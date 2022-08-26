498020c30bb1bf57609fd6369db97aed9a34ac9b
Charly77955 <desantiscarlos95@gmail.com>
2022-08-26 18:22:24 -0300

    feat: add icon to component img

--- inmoApp/src/gallery/Gallery.js
+++ inmoApp/src/gallery/Gallery.js
@@ -2,17 +2,27 @@ import React from 'react';
 import {Text, Image, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Inmuebles from '../data/Data.json';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faBed, faBath, faWarehouse} from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Gallery() {
   return (
     <>
-    
       {Inmuebles.Inmuebles.map((item, i) => (
         <SafeAreaView key={i} style={styles.container}>
+          <FontAwesomeIcon icon={faBed} 
+          style={styles.iconBed}/>
+          <FontAwesomeIcon icon={faBath} 
+          style={styles.iconBath}/>
+          <FontAwesomeIcon icon={faWarehouse} 
+          style={styles.warehouse}/>
           <ScrollView style={styles.scrollView}>
             <Text style={styles.textStyle}>{item.name}</Text>
             <TouchableOpacity>
+            
             <Image source={{uri: item.imagen}} alt="" style={styles.imgStyle} />
+            
             </TouchableOpacity>
           </ScrollView>
         </SafeAreaView>
@@ -40,12 +50,25 @@ const styles = StyleSheet.create({
     left: 20,
   },
   imgStyle: {
-    width: 270,
-    height: 180,
+    width: 150,
+    height: 140,
     margin: 10,
     flex: 1,
     left: 10,
     borderWidth: 1,
     borderColor: '#2a4944'
   },
+  iconBed:{
+     color:"#000000",
+     left: 230,
+     bottom: 100
+  },
+  iconBath: {
+    left: 280,
+    bottom: 117
+  },
+  warehouse:{
+    left: 320,
+    bottom: 130
+  }
 });
