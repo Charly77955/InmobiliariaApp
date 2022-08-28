5eb0b257786baa912ad5aabfd43fa8707792cbee
Charly77955 <desantiscarlos95@gmail.com>
2022-08-28 18:11:50 -0300

    feat: add text icons to component

--- inmoApp/src/gallery/Gallery.js
+++ inmoApp/src/gallery/Gallery.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Text, Image, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import {Text, Image, StyleSheet, ScrollView, TouchableOpacity, View, TextComponent} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Inmuebles from '../data/Data.json';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faBed, faBath, faWarehouse} from '@fortawesome/free-solid-svg-icons';
+import {faBed, faBath, faWarehouse, faLocationDot, faBookmark} from '@fortawesome/free-solid-svg-icons';
 
 
 export default function Gallery() {
@@ -11,20 +11,33 @@ export default function Gallery() {
     <>
       {Inmuebles.Inmuebles.map((item, i) => (
         <SafeAreaView key={i} style={styles.container}>
-          <FontAwesomeIcon icon={faBed} 
+          
+          <ScrollView style={styles.scrollView}>
+            <Text style={styles.textStyle}>{item.name}</Text>
+            <TouchableOpacity>
+            <Image source={{uri: item.imagen}} alt="" style={styles.imgStyle} />
+            <View style={styles.iconsContainer}>
+          
+              <Text style={styles.textBed}>3</Text>
+              <Text style={styles.textBath}>2</Text>
+              <Text style={styles.textWarehouse}>150m2</Text>
+              <Text style={styles.textUbication}>Dasso 2321,CABA</Text>
+          <FontAwesomeIcon icon={faBookmark} 
+          style={styles.iconfaBookmark}/>
+           <FontAwesomeIcon icon={faBed} 
           style={styles.iconBed}/>
           <FontAwesomeIcon icon={faBath} 
           style={styles.iconBath}/>
           <FontAwesomeIcon icon={faWarehouse} 
-          style={styles.warehouse}/>
-          <ScrollView style={styles.scrollView}>
-            <Text style={styles.textStyle}>{item.name}</Text>
-            <TouchableOpacity>
-            
-            <Image source={{uri: item.imagen}} alt="" style={styles.imgStyle} />
-            
+          style={styles.iconWarehouse}/>
+          <FontAwesomeIcon icon={faLocationDot} 
+          style={styles.iconLocation}/> 
+          </View>
             </TouchableOpacity>
+
           </ScrollView>
+          
+          
         </SafeAreaView>
       ))}
       
@@ -33,42 +46,94 @@ export default function Gallery() {
 }
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'space-between',
+        padding: 10,
+        alignItems: 'center',
         backgroundColor: "#4D5654"
     },
-  scrollView: {
-    marginHorizontal: 50,
-    backgroundColor: '#74807D',
-    flexDirection: 'column',
-    marginBottom: 20,
-   
+    scrollView :{  
+        flex: 1,
+        width: 340,
+        height: 210,
+        padding: 0,
+        flexWrap: 'wrap',
+        backgroundColor: '#74807D'
+    },
+    imgStyle: {
+      width: 170,
+      height: 150,
+      margin: 5,
+      flex: 1,
+      alignItems: 'flex-start',
+      borderWidth: 1,
+      borderColor: '#2a4944'
+    },
+    textStyle: {
+      fontSize: 15,
+      width:300,
+      paddingTop: 2,
+      color: 'white',
+      fontWeight: 'bold',
+      left: 5,
+    },
+    iconBed:{
+      color:"#000000",
+      left: 190,
+      bottom: 163,
+      width: 0,
+      height: 0
+   },
+   iconBath: {
+    left: 230,
+    bottom: 180,
+    width: 0,
+    height: 0
   },
-  textStyle: {
-    fontSize: 17,
-    paddingTop: 2,
-    color: 'white',
-    left: 20,
+  iconWarehouse:{ 
+    left: 270,
+    bottom: 195,
+    width: 0,
+    height: 0,
+    color:"#000000"
   },
-  imgStyle: {
-    width: 150,
-    height: 140,
-    margin: 10,
-    flex: 1,
-    left: 10,
-    borderWidth: 1,
-    borderColor: '#2a4944'
+  iconLocation:{
+    left: 180,
+    bottom: 280,
+    width: 0,
+    height: 0
   },
-  iconBed:{
-     color:"#000000",
-     left: 230,
-     bottom: 100
+  iconsContainer:{
+    width: 340,
+    height: 210
   },
-  iconBath: {
-    left: 280,
-    bottom: 117
+  textBed:{
+    left: 210,
+    color:"#000000",
+    bottom: 73,
+    fontWeight: 'bold'
   },
-  warehouse:{
+  textBath:{
+    left: 250,
+    bottom: 90,
+    color:"#000000",
+    fontWeight: 'bold'
+  },
+  textWarehouse: {
+    left: 290,
+    bottom: 110,
+    color:"#000000",
+    fontWeight: 'bold'
+  },
+  textUbication:{
+    left: 200,
+    bottom: 200,
+    color:"#000000",
+    fontWeight: 'bold'
+  },
+  iconfaBookmark:{
     left: 320,
-    bottom: 130
+    bottom: 90,
+    width: 0,
+    height: 0
   }
+  
 });
